668f80498d4ae256b763acf96b1b4a38a561d634
Circlewee <pnj1118@naver.com>
2022-03-28 16:40:31 +0900

    1.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './routes/Home.js';
 import Detail from './routes/Detail.js';
 import Nav from './components/Nav';
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         {/* home component */}
         <Route path='/ReactFlix' element={<Home />}></Route>
-        <Route path='/' element={<Navigate replace to='/ReactFlix' />} />
+        {/* <Route path='/' element={<Navigate replace to='/ReactFlix' />} /> */}
         {/* detail component */}
         {/* :(something)  ==>  useParams().(something) */}
         <Route path='/movie/:id' element={<Detail />}></Route>
